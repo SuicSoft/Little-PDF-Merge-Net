1efd62709bafa333ea2541ca3059c0d8e47d18b1
SuicSoft <suiciwd@gmail.com>
2015-08-26 13:09:15 +0530

    Fixed shortcut script

--- build.bat
+++ build.bat
@@ -1,17 +1,24 @@
+@echo off
+for %%* in (.) do title Building %%~nx*
+for %%* in (.) do echo Building %%~nx* ...
 msbuild /property:Configuration=Release "Little's PDF Merge.sln" /maxcpucount:%NUMBER_OF_PROCESSORS%
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET LinkName=Little's PDF Merge
-SET Esc_LinkDest=%%HOMEDRIVE%%%%HOMEPATH%%\Desktop\!LinkName!.lnk
-SET Esc_LinkTarget="Windows Desktop\Little's PDF Merge Windows\bin\Release\LPM.Windows.exe
-SET cSctVBS=CreateShortcut.vbs
-SET LOG=".\%~N0_runtime.log"
-((
-  echo Set oWS = WScript.CreateObject^("WScript.Shell"^) 
-  echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
-  echo Set oLink = oWS.CreateShortcut^(sLinkFile^) 
-  echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
-  echo oLink.Save
-)1>!cSctVBS!
-cscript //nologo .\!cSctVBS!
-DEL !cSctVBS! /f /q
-)1>>!LOG! 2>>&1
+for %%* in (.) do echo Creating Shortcut For %%~nx* ...
+Set MyFile=%~dp0\Windows Desktop\Little's PDF Merge Windows\bin\Release\LPM.Windows.exe
+Set ShorcutName=Little's PDF Merge
+(
+echo Call Shortcut("%MyFile%","%ShorcutName%"^)
+echo Sub Shortcut(ApplicationPath,Nom^)
+echo    Dim objShell,DesktopPath,objShortCut,MyTab
+echo    Set objShell = CreateObject("WScript.Shell"^)
+echo    MyTab = Split(ApplicationPath,"\"^)
+echo    If Nom = "" Then
+echo    Nom = MyTab(UBound(MyTab^)^)
+echo    End if
+echo    DesktopPath = objShell.SpecialFolders("Desktop"^)
+echo    Set objShortCut = objShell.CreateShortcut(DesktopPath ^& "\" ^& Nom ^& ".lnk"^)
+echo    objShortCut.TargetPath = Dblquote(ApplicationPath^)
+echo    objShortCut.Save
+echo End Sub
+echo Function DblQuote(Str^)
+echo    DblQuote = Chr(34^) ^& Str ^& Chr(34^)
+echo End Function
