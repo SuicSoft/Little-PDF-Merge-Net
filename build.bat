dd817cc6c653e898c6a851168888a54889ff09ca
SuicSoft <suiciwd@gmail.com>
2015-08-26 12:31:15 +0530

    Create desktop shortcut when building from source

--- build.bat
+++ build.bat
@@ -1,1 +1,17 @@
 msbuild /property:Configuration=Release "Little's PDF Merge.sln" /maxcpucount:%NUMBER_OF_PROCESSORS%
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET LinkName=Little's PDF Merge
+SET Esc_LinkDest=%%HOMEDRIVE%%%%HOMEPATH%%\Desktop\!LinkName!.lnk
+SET Esc_LinkTarget="Windows Desktop\Little's PDF Merge Windows\bin\Release\LPM.Windows.exe
+SET cSctVBS=CreateShortcut.vbs
+SET LOG=".\%~N0_runtime.log"
+((
+  echo Set oWS = WScript.CreateObject^("WScript.Shell"^) 
+  echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
+  echo Set oLink = oWS.CreateShortcut^(sLinkFile^) 
+  echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
+  echo oLink.Save
+)1>!cSctVBS!
+cscript //nologo .\!cSctVBS!
+DEL !cSctVBS! /f /q
+)1>>!LOG! 2>>&1
