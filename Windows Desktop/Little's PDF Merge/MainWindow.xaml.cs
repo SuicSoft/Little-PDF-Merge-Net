8e85d226585d1d9c049252267b386b844c7c4dcf
SuicSoft <suiciwd@gmail.com>
2015-09-12 08:09:11 +0530

    Code commenting

--- Windows Desktop/Little's PDF Merge/MainWindow.xaml.cs
+++ Windows Desktop/Little's PDF Merge/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using MahApps.Metro.Controls;
+﻿//For changing themes.
+using MaterialDesignColors;
 using MaterialDesignThemes.Wpf;
+//For the registry
 using Microsoft.Win32;
 using System;
+//For all the array stuff
 using System.Linq;
 
 namespace SuicSoft.LittlesPDFMerge.Windows
@@ -9,24 +12,30 @@ namespace SuicSoft.LittlesPDFMerge.Windows
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
+        /// <summary>
+        /// Initailizes the main window.
+        /// </summary>
         public MainWindow()
         {
+            //Load the UI.
             InitializeComponent();
             //Switch to dark if past 5:00 pm.
             if (DateTime.Now.TimeOfDay < new TimeSpan(7, 0, 0) | DateTime.Now.TimeOfDay > new TimeSpan(17, 0, 0))
                 new PaletteHelper().SetLightDark(true);
+            //Load the color palette from the registry.
+            LoadColors();
         }
-        protected override void OnSourceInitialized(EventArgs e)
+        /// <summary>
+        /// Loads the color values from the registry.
+        /// </summary>
+        private static void LoadColors()
         {
-            try
-            {
-                new PaletteHelper().ReplaceAccentColor(new MaterialDesignColors.SwatchesProvider().Swatches.ToList()[(int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\LittlePDFMerge", "Accent", 0)]);
-                new PaletteHelper().ReplacePrimaryColor(new MaterialDesignColors.SwatchesProvider().Swatches.ToList()[(int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\LittlePDFMerge", "Primary", 0)]);
-            }
-            catch { }
-            base.OnSourceInitialized(e);
+            //Load accent.
+            new PaletteHelper().ReplaceAccentColor(new SwatchesProvider().Swatches.ToList()[(int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\SuicSoft\\LittlePDFMerge", "Accent", 1)]);
+            //Load primary.
+            new PaletteHelper().ReplacePrimaryColor(new SwatchesProvider().Swatches.ToList()[(int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\SuicSoft\\LittlePDFMerge", "Primary", 9)]);
         }
     }
 }
--- Windows Desktop/Little's PDF Merge/View Models/PaletteSelectorViewModel.cs
+++ Windows Desktop/Little's PDF Merge/View Models/PaletteSelectorViewModel.cs
@@ -11,56 +11,96 @@ namespace SuicSoft.LittlesPDFMerge.Windows
 {
     public class PaletteSelectorViewModel
     {
+        #region Variables
+        /// <summary>
+        /// The index of the primary color
+        /// </summary>
+        public static int PrimaryIndex;
+        /// <summary>
+        /// The index if the accent color
+        /// </summary>
+        public static int AccentIndex;
+        /// <summary>
+        /// A list of all the Material Design swatches
+        /// </summary>
+        public static List<Swatch> Swatches { get; set; }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Save the color settings
+        /// </summary>
+        public ICommand SaveCommand { get; set; }
+        /// <summary>
+        /// Reset the color settings
+        /// </summary>
+        public ICommand ResetCommand { get; set; }
+        /// <summary>
+        /// Sets the accent color.
+        /// </summary>
+        public ICommand ApplyAccentCommand { get; set; }
+        /// <summary>
+        /// Sets the primary color
+        /// </summary>
+        public ICommand ApplyPrimaryCommand { get; set; }
+        /// <summary>
+        /// Sets light or dark
+        /// </summary>
+        public ICommand ToggleBaseCommand { get; set; }
+        #endregion
+
         public PaletteSelectorViewModel()
         {
             Swatches = new SwatchesProvider().Swatches.ToList();
-            ToggleBaseCommand = new ActionCommand(o => ApplyBase((bool)o));
+            ToggleBaseCommand = new ActionCommand(o => new PaletteHelper().SetLightDark((bool)o));
             ResetCommand = new ActionCommand(() => Reset());
             ApplyPrimaryCommand = new ActionCommand(o => ApplyPrimary((Swatch)o));
             ApplyAccentCommand = new ActionCommand(o => ApplyAccent((Swatch)o));
             SaveCommand = new ActionCommand(() => Save());
         }
-        public ICommand SaveCommand { get; set; }
-        public static int PrimaryIndex;
-        public static int AccentIndex;
+        /// <summary>
+        /// Save the color settings
+        /// </summary>
         private static void Save()
         {
-            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\LittlePDFMerge", "Accent", AccentIndex);
-            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\LittlePDFMerge", "Primary", PrimaryIndex);
+            //Save accent.
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\SuicSoft\\LittlePDFMerge", "Accent", AccentIndex);
+            //Save primary.
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\SuicSoft\\LittlePDFMerge", "Primary", PrimaryIndex);
         }
-        public ICommand ResetCommand { get; set; }
-
+        /// <summary>
+        /// Reset the color settings
+        /// </summary>
         private static void Reset()
         {
-            new PaletteHelper().ReplacePrimaryColor(Swatches[1]);
-            new PaletteHelper().ReplaceAccentColor(Swatches[9]);
-            PrimaryIndex = 1;
-            AccentIndex = 9;
+            //Replace primary with default.
+            ApplyPrimary(Swatches[1]);
+            //Replace accent with default.
+            ApplyAccent(Swatches[9]);
+            //Write the defaults to the registry.
             Save();
         }
-        public ICommand ToggleBaseCommand { get; set; }
-
-        private static void ApplyBase(bool isDark)
-        {
-            new PaletteHelper().SetLightDark(isDark);
-        }
-
-        public static List<Swatch> Swatches { get; set; }
-
-        public ICommand ApplyPrimaryCommand { get; set; }
-
+        /// <summary>
+        /// Sets the primary color.
+        /// </summary>
+        /// <param name="swatch">The primary color to set.</param>
         private static void ApplyPrimary(Swatch swatch)
         {
+            //Replace the color.
             new PaletteHelper().ReplacePrimaryColor(swatch);
+            //Set the color index
             PrimaryIndex = Swatches.FindIndex(x => x == swatch);
         }
-
-        public ICommand ApplyAccentCommand { get; set; }
-
+        /// <summary>
+        /// Sets the accent color.
+        /// </summary>
+        /// <param name="swatch">The accent color to set.</param>
         private static void ApplyAccent(Swatch swatch)
         {
-            new PaletteHelper().ReplaceAccentColor(swatch);
-            AccentIndex = Swatches.FindIndex(x => x == swatch);
+            //Replace the color.
+            new PaletteHelper().ReplacePrimaryColor(swatch);
+            //Set the color index
+            PrimaryIndex = Swatches.FindIndex(x => x == swatch);
         }
     }
 }
