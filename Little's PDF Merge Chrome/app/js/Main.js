addca2f8c01e7fc4ff64e670f86c4ff3d552abe2
SuicSoft <suiciwd@gmail.com>
2015-09-19 14:45:44 +0530

    scrongle

---
+++ Little's PDF Merge Chrome/app/js/Main.js
@@ -0,0 +1,16 @@
+﻿//<![CDATA[
+$(function () {
+    $('#addbtn').on('click', function () {
+        $('#file').trigger('click');
+    });
+    $('#closebtn').on('click', function () {
+        window.close();
+    });
+    $('#minbtn').on('click', function () {
+        chrome.app.window.current().minimize();
+    });
+
+    $('#maxbtn').on('click', function () {
+        chrome.app.window.current().maximize();
+    });
+});//]]>
