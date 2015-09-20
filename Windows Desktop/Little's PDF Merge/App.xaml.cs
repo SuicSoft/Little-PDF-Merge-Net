64dab6bc995cf42fd9eaa9d68126e21809705205
SuicSoft <suiciwd@gmail.com>
2015-09-20 12:42:57 +0530

    Added updates

--- Windows Desktop/Little's PDF Merge/App.xaml.cs
+++ Windows Desktop/Little's PDF Merge/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Squirrel;
+using System.Windows;
 namespace SuicSoft.LittlesPDFMerge.Windows
 {
     /// <summary>
@@ -6,9 +7,20 @@ namespace SuicSoft.LittlesPDFMerge.Windows
     /// </summary>
     public partial class App : Application
     {
+        static bool ShowTheWelcomeWizard;
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-
+            if (System.Diagnostics.Debugger.IsAttached) return;
+            using (var mgr = new UpdateManager("http://suicsoft.com/updates/lpm/"))
+            {
+                // Note, in most of these scenarios, the app exits after this method
+                // completes!
+                SquirrelAwareApp.HandleEvents(
+                  onInitialInstall: v => mgr.CreateShortcutForThisExe(),
+                  onAppUpdate: v => mgr.CreateShortcutForThisExe(),
+                  onAppUninstall: v => mgr.RemoveShortcutForThisExe(),
+                  onFirstRun: () => ShowTheWelcomeWizard = true);
+            }
         }
     }
 }
