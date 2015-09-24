a8b5f9d651ae4793b67ff4b6f27c5661ee878141
SuicSoft <suiciwd@gmail.com>
2015-09-24 12:30:35 +0530

    Added plugin files

---
+++ Plugins/Little's PDF Merge Achievements/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.ComponentModel.Composition;
+namespace SuicSoft.LittlesPDFMerge.Achievements
+{
+    public class Plugin : SuicSoft.LittlesPDFMerge.PluginBase.IPlugin //IPlugin
+    {
+        public string Name { get; set; }
+        public BitmapImage icon { get; set; }
+        public string Author { get; set; }
+        public Version Version { get; set; }
+        public void Settings()
+        {
+
+        }
+        public void OnAppInit()
+        {
+
+        }
+        public void OnLoad()
+        {
+            MessageBox.Show("Hello World");
+        }
+        public void OnUILoad()
+        {
+
+        }
+        public void OnRemove()
+        {
+
+        }
+        public void OnMerge()
+        {
+
+        }
+        public void OnUp()
+        {
+
+        }
+        public void OnDown()
+        {
+
+        }
+    }
+}
---
+++ Windows Desktop/Little's PDF Merge/GenericMEFPluginLoader.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * File Name : GenericMEFPluginLoader.cs
+ * Language : C#.NET (.NET 4.5)
+ * Description : Loads Managed Extensibility Framework (MEF) https://msdn.microsoft.com/en-us/library/dd460648(v=vs.110).aspx from a folder.
+ * Source : https://code.msdn.microsoft.com/windowsdesktop/Creating-a-simple-plugin-b6174b62/sourcecode?fileId=74454&pathId=1812241599
+ */
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+namespace SuicSoft.LittlesPDFMerge.Windows
+{
+    public class GenericMEFPluginLoader<T>
+    {
+        private CompositionContainer _Container;
+
+        /// <summary>
+        /// A list of all the MEF plugins installed
+        /// </summary>
+        [ImportMany]
+        public System.Collections.Generic.IEnumerable<T> Plugins {get; set;}
+        /// <summary>
+        /// Initailizes the plugin loader
+        /// </summary>
+        /// <param name="path">The path to load plugins from</param>
+        public GenericMEFPluginLoader(string path)
+        {
+            DirectoryCatalog directoryCatalog = new DirectoryCatalog(path);
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog(directoryCatalog);
+            // Create the CompositionContainer with all parts in the catalog (links Exports and Imports)
+            _Container = new CompositionContainer(catalog);
+            //Fill the imports of this object
+            _Container.ComposeParts(this);
+        }
+    }
+}
